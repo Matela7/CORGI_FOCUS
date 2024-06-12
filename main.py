42f8804f2c3529cdf71a811dcdb0a249cfbdce9e
Matela <michexstudio@gmail.com>
2024-06-12 23:17:55 +0200

    dasasdads

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ from kivy.uix.image import Image
 from kivy.uix.label import Label
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.button import Button
-from functools import partial  # Importujemy funkcję partial z modułu functools
+from kivy.uix.scrollview import ScrollView
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.widget import Widget
+from functools import partial
 
 sites_to_block = ["facebook.com", "www.facebook.com", "instagram.com", "www.instagram.com"]
 hosts_path = r"C:\Windows\System32\drivers\etc\hosts"  # Windows
@@ -34,22 +37,43 @@ class FirstScreen(Screen):
         super().__init__(**kwargs)
         layout = BoxLayout(orientation='vertical')
 
-        text = Label(text="CORGI FOCUS", font_size='30sp', size_hint=(1, 0.3))
+        text = Label(text="CORGI FOCUS", font_size='30sp', size_hint=(1, 0.2))
         layout.add_widget(text)
 
-        text2 = Label(text="Stop procastination with CORGI!\nTurn on focus mode to BLOCK addicting websites\nto add websites or delete them, turn second page,\nin case to stop blocking websites, just click again", font_size='20sp', size_hint=(1, 0.3))
-        layout.add_widget(text2)
+        horizontal_layout = BoxLayout(orientation='horizontal', size_hint=(1, 0.5))
+        
+        text2 = Label(text="Stop procastination with CORGI!\nTurn on focus mode to BLOCK addicting websites\nto add websites or delete them, turn second page,\nin case to stop blocking websites, just click again", font_size='25sp', size_hint=(0.5, 1), halign='left', valign='middle')
+        horizontal_layout.add_widget(text2)
 
-        image = Image(source='korki.gif', size_hint=(1, 0.4))
-        layout.add_widget(image)
+        image = Image(source='korki.gif', size_hint=(0.5, 1))
+        horizontal_layout.add_widget(image)
 
-        button_block = Button(text="BLOCK", font_size='20sp', size_hint=(1, 0.1))
+        layout.add_widget(horizontal_layout)
+
+        buttons_layout = BoxLayout(orientation='horizontal', size_hint=(1, 0.2))
+
+        button_block = Button(text="BLOCK", font_size='20sp', size_hint=(0.1, 0.5))
         button_block.bind(on_press=partial(self.on_block_button_press))
-        layout.add_widget(button_block)
+        buttons_layout.add_widget(button_block)
 
-        button_unblock = Button(text="UNBLOCK", font_size='20sp', size_hint=(1, 0.1))
+        spacer1 = Widget(size_hint=(0.1, 1))
+        buttons_layout.add_widget(spacer1)
+
+        button_unblock = Button(text="UNBLOCK", font_size='20sp', size_hint=(0.1, 0.5))
         button_unblock.bind(on_press=partial(self.on_unblock_button_press))
-        layout.add_widget(button_unblock)
+        buttons_layout.add_widget(button_unblock)
+
+        spacer2 = Widget(size_hint=(0.1, 1))
+        buttons_layout.add_widget(spacer2)
+
+        button_next = Button(text="Next", font_size='20sp', size_hint=(0.1, 0.5))
+        button_next.bind(on_press=self.on_next_button_press)
+        buttons_layout.add_widget(button_next)
+
+
+
+        layout.add_widget(buttons_layout)
+
 
         self.add_widget(layout)
         
@@ -58,11 +82,43 @@ class FirstScreen(Screen):
 
     def on_unblock_button_press(self, instance):
         unblock_sites(hosts_path, sites_to_block, redirect_ip)
+        
+    def on_next_button_press(self, instance):
+        self.manager.current = 'second'
+
+class SecondScreen(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        layout = BoxLayout(orientation='vertical')
+
+        text = Label(text="Manage Websites", font_size='30sp', size_hint=(1, 0.1))
+        layout.add_widget(text)
+
+        scrollview = ScrollView(size_hint=(1, 0.8))
+        grid = GridLayout(cols=1, size_hint_y=None)
+        grid.bind(minimum_height=grid.setter('height'))
+
+        for site in sites_to_block:
+            lbl = Label(text=site, font_size='20sp', size_hint_y=None, height=40)
+            grid.add_widget(lbl)
+
+        scrollview.add_widget(grid)
+        layout.add_widget(scrollview)
+
+        button_prev = Button(text="Previous", font_size='20sp', size_hint=(1, 0.1))
+        button_prev.bind(on_press=self.on_prev_button_press)
+        layout.add_widget(button_prev)
+
+        self.add_widget(layout)
+        
+    def on_prev_button_press(self, instance):
+        self.manager.current = 'first'
 
 class MyApp(App):
     def build(self):
         screen_manager = ScreenManager()
         screen_manager.add_widget(FirstScreen(name='first'))
+        screen_manager.add_widget(SecondScreen(name='second'))
         return screen_manager
 
 if __name__ == '__main__':
