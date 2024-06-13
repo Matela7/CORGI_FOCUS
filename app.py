b54f1f6e14129f4ddf0b87fb0a867a07237e9812
Matela <michexstudio@gmail.com>
2024-06-13 22:23:43 +0200

    dassdas

--- app.py
+++ app.py
@@ -1,4 +1,17 @@
 import streamlit as st
 
-st.title("Strona zablokowana")
-st.write("Próbowałeś uzyskać dostęp do zablokowanej strony.")
+st.set_page_config()
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.header("          BIG CORGI IS WATCHING!")
+    st.write("Stop procrastination with CORGI!")
+    st.write("Who controls the past controls the future.")
+    st.write("Who controls the present controls the past.")
+    st.write("War is peace.")
+    st.write("Freedom is slavery.")
+with col2:
+    st.image("1.jpg", use_column_width=True, )
+
+
